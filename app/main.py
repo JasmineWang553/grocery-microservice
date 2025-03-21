612599756b3c6ddc0d44ec6052b40132996dca5b
Jasmine Wang <jasminewang6026@gmail.com>
2025-03-21 14:13:10 -0600

    commit with monitor metrics

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from app.routes import router
 from app.database import grocery_collection  # Make sure this is your DB collection
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Grocery List API", description="FastAPI MongoDB Example", version="1.0.0")
 
 app.include_router(router)
+Instrumentator().instrument(app).expose(app)
 
 def clear_test_data():
     """Clears all items from the database to ensure a fresh state for tests."""
